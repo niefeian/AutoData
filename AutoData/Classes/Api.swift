38f2c0c5e05e11656c4971aa069cb1210e5cda0d
872447993@qq.com <872447993@qq.com>
2019-11-08 16:29:54 +0800

    bug

--- AutoData/Classes/Api.swift
+++ AutoData/Classes/Api.swift
@@ -12,37 +12,36 @@ open class Api {
    
     public static let POST = "POST"
     public static let GET = "GET"
-    
+
     public static var GETHOST = ""
     public static var HOST =  ""
     public static var WEB_HOST =  ""
     public static var DebugHOST =  ""
     public static var DebugWEB_HOST =  ""
-    
-    class func WebHost() -> String {
-        if isDebug {
-            return DebugWEB_HOST
-        }
-        return WEB_HOST
+
+    public class func WebHost() -> String {
+       if isDebug {
+           return DebugWEB_HOST
+       }
+       return WEB_HOST
     }
-    
-//    zw.flany.cn zw.feiyo.com
+
     public class func BaseHost() -> String {
-        if isDebug {
-            return DebugHOST
-        }
-        return HOST
+       if isDebug {
+           return DebugHOST
+       }
+       return HOST
     }
-    
-    
-    class func Web_PREFIX(_ url : String) -> String {
-        if !url.hasPrefix("http") {
-            return (WebHost() + url)
-        }
-        return url
+
+
+    public class func Web_PREFIX(_ url : String) -> String {
+       if !url.hasPrefix("http") {
+           return (WebHost() + url)
+       }
+       return url
     }
-    
-    
+
+
     public static var commonPara : [String:String]!
     public static var josnKey : String = ""//这个是用来保存后端下发的数据的一种简易方式
 }
